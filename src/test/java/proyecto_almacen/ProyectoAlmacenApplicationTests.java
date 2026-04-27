e556465d837e36ffa470c26971171c80b63227b5
María Paz Noreña Aguirre <165820078+MeryNore@users.noreply.github.com>
2026-04-27 09:12:20 +0200

    primer comit

---
+++ src/test/java/proyecto_almacen/ProyectoAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
