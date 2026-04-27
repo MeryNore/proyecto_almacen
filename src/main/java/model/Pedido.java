aebcf699bfb0c7d196a2c10636b7848b0b0eca3c
melisatieneprisa <melisamorenocastillo@gmail.com>
2026-04-27 09:56:49 +0200

    dto pedido

--- src/main/java/model/Pedido.java
+++ src/main/java/model/Pedido.java
@@ -1,5 +1,41 @@
 package model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Pedido {
-    //holaaaa
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cliente;
+    private LocalDate fecha;
+
+    //El lado dueño es el Pedido, por lo que creamos la tabla de relación n:m aquí
+    //Tendrá su HashSet de productos
+    @ManyToMany
+    @JoinTable(
+            name = "pedido_producto",
+            joinColumns = @JoinColumn(name = "pedido_id"),
+            inverseJoinColumns = @JoinColumn(name = "producto_id")
+    )
+    private Set<Producto> productos = new HashSet<>();
+
+    //Getters y setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getCliente() { return cliente; }
+    public void setCliente(String cliente) { this.cliente = cliente; }
+
+    public LocalDate getFecha() { return fecha; }
+    public void setFecha(LocalDate fecha) { this.fecha = fecha; }
+
+    public Set<Producto> getProductos() { return productos; }
+    public void setProductos(Set<Producto> productos) { this.productos = productos; }
 }
