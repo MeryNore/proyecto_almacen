e3a1d54a89a8be9ec9e354b74eb807e1ff549c51
María Paz Noreña Aguirre <165820078+MeryNore@users.noreply.github.com>
2026-04-28 09:35:57 +0200

    añadir Servicios productos y pedidos

--- src/main/java/service/PedidoService.java
+++ src/main/java/service/PedidoService.java
@@ -1,4 +1,51 @@
 package service;
 
+import model.Pedido;
+import model.Producto;
+import repository.PedidoRepository;
+import repository.ProductoRepository;
+
+import java.util.List;
+
+
 public class PedidoService {
+
+    private PedidoRepository pedidoRepo;
+    private ProductoService productoService;
+
+    public PedidoService(PedidoRepository pedidoRepo, ProductoService productoService){
+        this.pedidoRepo = pedidoRepo;
+        this.productoService = productoService;
+    }
+
+    public List<Pedido> obtenerTodo(){
+        return pedidoRepo.findAll();
+    }
+
+    public Pedido obtenerPorId(Long id){
+        return pedidoRepo.findById(id).orElseThrow(() -> new RuntimeException("Pedido no encontrado: " + id));
+    }
+
+    public void guardar(Pedido pedido){
+        pedidoRepo.save(pedido);
+    }
+
+    public void eliminar(Long id){
+        pedidoRepo.deleteById(id);
+    }
+
+    public void asignarProducto(Long pedidoId, Long productoId) {
+        Pedido pedido = obtenerPorId(pedidoId);
+        Producto producto = productoService.obtenerPorId(productoId);
+        pedido.getProductos().add(producto);
+        pedidoRepo.save(pedido);
+    }
+
+    public void desasignarProduco(Long pedidoId, Long productoId){
+        Pedido pedido = obtenerPorId(pedidoId);
+        pedido.getProductos().removeIf(p -> p.getId().equals(productoId));
+
+    }
+
+
 }
--- src/main/java/service/ProductoService.java
+++ src/main/java/service/ProductoService.java
@@ -1,4 +1,36 @@
 package service;
 
+import model.Pedido;
+import model.Producto;
+import repository.PedidoRepository;
+import repository.ProductoRepository;
+
+import java.util.List;
+
 public class ProductoService {
+
+    private PedidoService pedidoService;
+    private ProductoRepository productoRepo;
+
+    public ProductoService(PedidoService pedidoService, ProductoRepository productoRepo){
+        this.pedidoService = pedidoService;
+        this.productoRepo = productoRepo;
+    }
+
+    public List<Producto> obtenerTodo(){
+        return productoRepo.findAll();
+    }
+
+    public Producto obtenerPorId(Long id){
+        return productoRepo.findById(id).orElseThrow(() -> new RuntimeException("Pedido no encontrado: " + id));
+    }
+
+    public void guardar(Producto producto){
+        productoRepo.save(producto);
+    }
+
+    public void eliminar(Long id){
+        productoRepo.deleteById(id);
+    }
+
 }
