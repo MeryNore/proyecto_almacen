ce948d34723d5a040ac87e014ef8d9a0a4900df3
María Paz Noreña Aguirre <165820078+MeryNore@users.noreply.github.com>
2026-04-29 08:38:58 +0200

    inicio controller producto

--- src/main/java/controller/ProductoController.java
+++ src/main/java/controller/ProductoController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import service.ProductoService;
+
+
+@Controller
 public class ProductoController {
+
+    private final ProductoService service;
+
+    public ProductoController(ProductoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/producto")
+    public String
+
+
 }
